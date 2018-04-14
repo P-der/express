a8c2f3fb258f77aa05635697ecb235aaf3f95e25
P-der <1198811801@qq.com>
2018-04-14 11:03:47 +0800

    1103

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var AV = require('leanengine');
 
+AV.init({
+  appId: process.env.LEANCLOUD_APP_ID || 'oE9MlGHXuhoY80lTsFB1Odkw-gzGzoHsz',
+  appKey: process.env.LEANCLOUD_APP_KEY || 'JU4a5fm9ltMgQ7D3BeXDYehV',
+  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || '0Tq6dJNvYA9a8lUSeMs1gBHU'
+});
 var app = express();
-
+app.use(AV.express());
 app.get("/",function(req,res){
     res.end('123');
 })
-
-app.listen(3000);
+app.listen(process.env.LEANCLOUD_APP_PORT);
